7ab28843d7c5b23ffa3a8435b34f16e201cf432f
anaphamali <anaphamali@gmail.com>
2013-12-06 15:16:47 +0100

    mocki klas odpowiedzialnych za czcionki

--- converter.py
+++ converter.py
@@ -1,6 +1,19 @@
 def getText():
     return 'Git'
 
+class Font:
+    pass
+
+class FontLoader:
+    def loadFont(self, directory):
+        pass
+
+t = getText()
+f = FontLoader().loadFont('fancyFont/')
+
+def getText():
+    return 'Git'
+
 t = getText()
 f = FontLoader().loadFont('fancyFont/')
 
