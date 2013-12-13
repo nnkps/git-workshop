91605f4584da19d0ab4dca35b85e96c16aae57c2
Anna Kupś <kups@student.agh.edu.pl>
2013-12-13 15:01:25 +0100

    czcionki wydzielone do plikow

--- font.py
+++ font.py
@@ -6,23 +6,4 @@ class Font:
 class FontLoader:
     def loadFont(self, directory):
         font = Font("fancy")
-
-        font.letters['G'] = r'''
-  ___
- / __)
-( (_ \
- \___/
-'''
-        font.letters['I'] = r'''
-  __
- (  )
-  )(
- (__)
-'''
-        font.letters['T'] = r'''
- ____
-(_  _)
-  )(
- (__)
-'''
         return font
