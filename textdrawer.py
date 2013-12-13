e13bd41727b1bac9ef0dd739c48ca8a6731c74cb
anaphamali <anaphamali@gmail.com>
2013-12-13 15:00:22 +0100

    funkcja sklejajaca literki poziomo

--- textdrawer.py
+++ textdrawer.py
@@ -3,7 +3,13 @@ class TextDrawer:
         self.font = font
 
 	
-    def draw(self, text):
+     def draw(self, text):
         for character in text:
             print self.font.letters[character.upper()]
 
+
+def appendLetter(text, letter):
+    for i in xrange(len(text)):
+        text[i] += letter[i]
+
+    return text
